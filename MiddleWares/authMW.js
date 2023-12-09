5741de67bf36235532f46f2c9320ec331cfe9332
m7moud emam <m7moud_emam@hotmail.com>
2023-12-10 01:06:36 +0200

    update MwAuth to check the role of the user

--- MiddleWares/authMW.js
+++ MiddleWares/authMW.js
@@ -1,43 +1,75 @@
-const jwt  = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const Student = mongoose.model('student');
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const Student = mongoose.model("student");
 
+const auth = async (req, res, next) => {
+  const token = String(req.get("Authorization")).split();
 
-const auth = async(req , res, next)=>{
-    const token = String (req.get("Authorization")).split();
+  try {
+    const decoded = jwt.verify(token[0], process.env.SECRET_KEY);
 
-    try{
+    // if(decoded.role == "user"){
+    //     try{
+    //         const user = await User.findOne({email:decoded.user.email});
 
-        const decoded = jwt.verify(token[0] , process.env.SECRET_KEY);
+    //         if(!user){
+    //             throw new Error();
+    //         }
+    //         req.user = user;
+    //         req.role = decoded.role;
+    //         next();
 
-        if(decoded.role == "user"){
-            try{
-                const user = await User.findOne({email:decoded.user.email});
+    //     }
+    //     catch(err){
+    //         res.status(401).send({error:"User Is Not Authenticated"});
+    //     }
+    // }
 
-                if(!user){
-                    throw new Error();
-                }
-                req.user = user;
-                req.role = decoded.role;
-                next();
+    // else if(decoded.role == ("admin")){
+    //     req.email = decoded.email;
+    //     req.role = decoded.role;
+    //     next();
+    // }
 
-            }
-            catch(err){
-                res.status(401).send({error:"User Is Not Authenticated"});
-            }
+    if (decoded.role) {
+      if (decoded.role == "admin") {
+        // Admin authentication
+        req.email = decoded.email;
+        req.role = decoded.role;
+        next();
+      }else{
+        try{
+            const student = await Student.findOne({ email: decoded.email });
+
+            if (!student) {
+                throw new Error();
+              }
+
+              req.user = student;
+              next();
         }
+        catch(err){
+            res.status(401).send({ error: 'User Is Not Authenticated' });
+        }
+      }
+    } else {
 
-        else if(decoded.role == ("admin")){
-            req.email = decoded.email;
-            req.role = decoded.role;
+        try {
+            const student = await Student.findOne({ email: decoded.email });
+    
+            if (!student) {
+              throw new Error();
+            }
+    
+            req.user = student;
             next();
-        }
-
+          } catch (err) {
+            res.status(401).send({ error: 'User Is Not Authenticated' });
+          }
     }
-    catch(err){
-        res.status(401).send({error:"User Is Not Authenticated"})
-    }
-}
-
+  } catch (err) {
+    res.status(401).send({ error: "User Is Not Authenticated" });
+  }
+};
 
 module.exports = auth;
